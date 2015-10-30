0c59da321519864d3e50586a1bd20f9b7979c79c
Jong Woo Nam <jwnam0709@gmail.com>
2015-10-30 14:54:40 -0400

    git interactive adding practice

--- hello.py
+++ hello.py
@@ -2,3 +2,8 @@ print "hello Software design git toolbox!!"
 
 print "uncommitting practice"
 print "rebase using pick commit"
+print "interactive adding practice"
+
+for i in range(5):
+	print "1\n"*i
+
